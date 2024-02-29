439df0e157bf6478ea112d2538bb412dff406f8e
Muhammet Erol <muhammederol971@gmail.com>
2024-02-29 14:51:25 +0100

    some bug fixed

--- checkout.js
+++ checkout.js
@@ -13,7 +13,16 @@ deleteallbtn.addEventListener("click", () => {
 products.addEventListener("click", (event) => {
   if (event.target.classList.contains("fa-plus")) {
     event.target.previousElementSibling.textContent++;
-    calculatePrice(event.target)
+    calculatePrice(event.target);
+  } else if (event.target.classList.contains("fa-minus")) {
+    if (event.target.nextElementSibling.textContent <= 0) {
+      return;
+    }
+    // 17 satır ile 19 değişince neden çalışmıyor derste 2.49 da saatte kaldın
+    event.target.nextElementSibling.textContent--;
+    calculatePrice(event.target);
+  } else if (event.target.classList.contains("fa-trash-can")) {
+    event.target.closest(".product").remove();
   }
 });
 
@@ -22,5 +31,12 @@ const calculatePrice = (dis) => {
   const discountendPrice = dis
     .closest(".product-info")
     .querySelector("#discounted-price");
-    discountendPrice.textContent
+
+  const productPrice = dis
+    .closest(".buttons-div")
+    .querySelector("#product-price");
+  const quantity = dis.parentNode.querySelector("#quantity");
+  productPrice.textContent = (
+    quantity.textContent * discountendPrice.textContent
+  ).toFixed(2);
 };
